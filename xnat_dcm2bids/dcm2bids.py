2ee2d4d048b7914bb869d479b23487da24406787
Bartosz Kossowski <b.kossowski@nencki.edu.pl>
2025-10-22 11:04:33 +0200

    adapt to multiple sessions

--- xnat_dcm2bids/dcm2bids.py
+++ xnat_dcm2bids/dcm2bids.py
@@ -19,11 +19,9 @@ def run_dcm2bids(session_ids, subject, session, config, output_dir, sourcedata, 
         "-p", subject,
         "-s", session,
         "-c", config,
-        "-o", output_dir
-    ]
-
-    for session_id in session_ids:
-        cmd.extend(["-d", str(sourcedata / session_id)])
+        "-o", output_dir,
+        "-d"
+    ] + [str(sourcedata / s) for s in session_ids]
 
     if auto_extract:
         cmd.insert(1, "--auto_extract_entities")
